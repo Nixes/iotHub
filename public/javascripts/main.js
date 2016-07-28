719c8a5d2befa69d9c2f21d0f77845262a5e2920
Nixes <callum@see-bry.com>
2016-07-28 23:09:42 +1000

    initial angularjs setup

---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+// Define the `phonecatApp` module
+var iotHub = angular.module('iotHub', []);
+
+iotHub.controller('OverviewController', function OverviewController($scope) {
+  $scope.sensors = [ ];
+}
+
+
+);
