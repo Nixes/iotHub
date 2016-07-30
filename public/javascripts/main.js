832c23c7935d603176f161d90c3e627b78542ac3
Nixes <randomdude7@gmail.com>
2016-07-30 17:38:14 +1000

    add charting libraries

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,19 @@
+// enable graphing module
+angular.module('myModule', ['chart.js']);
+
 // Define the `phonecatApp` module
 var iotHub = angular.module('iotHub', []);
 
 iotHub.controller('OverviewController', function OverviewController($scope, $http) {
   $http.get('./api/sensors').success(function(data) {
     $scope.sensors = data;
-
   });
 
 });
 
 
-iotHub.controller('HistoricController', function OverviewController($scope, $http) {
-  $http.get('./api/sensors').success(function(data) {
-    $scope.sensors = data;
-
+iotHub.controller('SensorsController', function OverviewController($scope, $http) {
+  $http.get('./api/sensors').success(function(sensors) {
+    $scope.sensors = sensors;
   });
-
 });
