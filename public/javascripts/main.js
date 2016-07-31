f3420488d513502533c1c96cccf276e32a137c3e
Nixes <callum@see-bry.com>
2016-07-31 12:30:27 +1000

    add sensor id to selection list

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,9 @@
 // enable graphing module
 angular.module('myModule', ['chart.js']);
 
-// Define the `phonecatApp` module
+// define the iotHub module so we can use it
 var iotHub = angular.module('iotHub', []);
+
 
 iotHub.controller('OverviewController', function OverviewController($scope, $http) {
   $http.get('./api/sensors').success(function(data) {
