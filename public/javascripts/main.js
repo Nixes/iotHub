22da9e73d1fff33cf7b7db783ef5df8000e1c86e
Nixes <callum@see-bry.com>
2016-08-01 13:46:06 +1000

    try to build a minimal test jig for angular chart

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,3 +18,32 @@ iotHub.controller('SensorsController', function OverviewController($scope, $http
   });
   $scope.model = null;
 });
+
+
+iotHub.controller("LineChart", function ($scope) {
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+});
