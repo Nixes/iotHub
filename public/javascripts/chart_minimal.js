960613942801ab0505a3e9a3e8d52e613dc8c3a8
Nixes <callum@see-bry.com>
2016-08-01 16:53:23 +1000

    extra minimal

---
+++ public/javascripts/chart_minimal.js
@@ -0,0 +1,41 @@
+// define the iotHub module so we can use it
+var iotHub = angular.module('iotHub', ['chart.js']);
+
+iotHub.controller('LineCtrl', ['$scope', function ($scope) {
+  $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.onHover = function (points) {
+    if (points.length > 0) {
+      console.log('Point', points[0].value);
+    } else {
+      console.log('No point');
+    }
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+}]);
