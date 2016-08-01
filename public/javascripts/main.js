de93dedac5553c6fe820615ea4a772a4e8fb09be
Nixes <callum@see-bry.com>
2016-08-01 17:15:39 +1000

    convert graph to time series

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,12 +17,16 @@ iotHub.controller('SensorsController', function OverviewController($scope, $http
 });
 
 iotHub.controller('LineCtrl', ['$scope', function ($scope) {
-  $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  $scope.data =  [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }];
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
@@ -33,24 +37,13 @@ iotHub.controller('LineCtrl', ['$scope', function ($scope) {
       console.log('No point');
     }
   };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
 
   $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
         }
-      ]
-    }
   };
 }]);
