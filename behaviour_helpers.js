4ef562bceada7c12fead4c363cd885e531f158b1
Nixes <callum@see-bry.com>
2017-01-19 17:26:37 +1100

    add functions to check for behaviours on new data addition, and then evaluate them

--- behaviour_helpers.js
+++ behaviour_helpers.js
@@ -1,10 +1,10 @@
 var mongoose = require( 'mongoose' );
 var Behaviour = mongoose.model('Behaviour');
+var Actor = mongoose.model('Actor');
 
 var behaviour_helpers = {};
 
 // this entire module is designed to allow the
-
 behaviour_helpers.condition_list_string = [
   "equal",
   "not equal",
@@ -72,8 +72,42 @@ behaviour_helpers.ConditionalToString = function (condition_num) {
   return behaviour_helpers.condition_list_string[condition_num];
 };
 
-behaviour_helpers.ConditionalToEval = function (conditional, value, state) {
+behaviour_helpers.EvaluateConditional = function (conditional, value, state) {
   return behaviour_helpers.condition_list_functions[conditional](value, state);
 };
+
+// run an action as found in a behaviour, TODO: Complete
+behaviour_helpers.PerformAction = function (action, actor) {
+  console.log("Action " + action + " to be performed by "+actor);
+};
+
+behaviour_helpers.Validate = function (behaviour){
+  // check that number of the conditional type is within available options
+  if (behaviour.condition > behaviour_helpers.condition_list_functions.length) {return false;}
+
+  return true;
+};
+
+behaviour_helpers.CheckBehaviour = function(sensor_id,last_sensor_state) {
+  Behaviour.find({sensor:sensor_id}).lean().exec( function(err,data){
+    if (err) {
+      console.log("Unable to find matching behaviour");
+      return;
+    } else {
+      console.log("Found a matching behaviour, validating");
+      console.log("Actor found within was: ");
+      console.log(data.actor);
+      // validate the behaviour first
+      if (behaviour_helpers.Validate(data)) {
+        console.log("Passed, testing");
+        if (behaviour_helpers.EvaluateCondition(data.condition,data.value,last_sensor_state) ) {
+          console.log("Passed, running action");
+          behaviour_helpers.PerformAction(data.action,data.actor);
+        }
+      }
+    }
+  });
+};
+
 
 module.exports = behaviour_helpers;
