c0a71ea25e18486617d1975aba190223f7cd989d
Nixes <callum@see-bry.com>
2017-01-23 14:20:10 +1100

    move helper methods to dedicated actor_helpers

---
+++ actor_helpers.js
@@ -0,0 +1,67 @@
+var mongoose = require( 'mongoose' );
+var Actor = mongoose.model('Actor');
+
+var actor_helpers = {};
+
+actor_helpers.CheckActorExists = function(actor_id,callback,res) {
+  Actor.findById(actor_id, function(err,actor){
+    if (err || !actor) {
+      console.log("Actor not registered err: "+err);
+      if (res) res.status(404).json({success:false,error:"Actor not registered"});
+    } else {
+      callback(actor);
+    }
+  });
+};
+
+// this function should be run everytime the actor interacts with the hub
+actor_helpers.ActorInteraction = function(actor_id, res) {
+  actor_helpers.CheckActorExists(actor_id, function(actor) {
+    actor.last_seen_time = new Date();
+    actor.save(function(err,actor) {
+      if (err) {
+        console.log("Failed to update last seen time err: "+err);
+        if (res) res.json({success:false});
+      } else {
+        if (res) res.json({success:true});
+      }
+    });
+  });
+};
+
+actor_helpers.PerformAction = function (actor, action, res) {
+  if (actor.state == req.body.state) return; // only process when something changed
+  // TODO: check that received state type matches the state_type of the model
+
+  // change the state of the actor itself
+  request.post('http://' + actor.last_seen_host + '/actor/' + actor._id + '',{ json: {state:action} }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+
+      // if all goes well then update the db with the new state
+      console.log("Updating actor " + actor._id + " state to " + req.body.state );
+      actor.state = action;
+      // handle issues with conversion
+      actor.save(function(err,actor) {
+        if (err) {
+          console.log("Failed to update local actor state err: "+err);
+          if (res) res.status(404).json({success:false});
+        } else {
+          if (res) res.json({success:true});
+        }
+      });
+    } else {
+      console.log("Failed to update remote actor state");
+      if (res) res.status(404).json({success:false});
+    }
+  });
+};
+
+actor_helpers.Validate = function (actor){
+
+  return true;
+};
+
+
+
+module.exports = actor_helpers;
--- routes/api_actors.js
+++ routes/api_actors.js
@@ -2,67 +2,13 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
 var request = require('request'); // request api is required for sending posts to the actors internal server
+var actor_helpers = require('../actor_helpers'); // add helpers for conditional support
 
 // include models
 var Actor = mongoose.model('Actor');
 var Data = mongoose.model('Data');
 var Overview = mongoose.model('Overview');
 
-// this function is called everytime a new actor state is received
-function updateActorState(actor,req,res){
-  if (actor.state == req.body.state) return; // only process when something changed
-  // TODO: check that received state type matches the state_type of the model
-
-  // change the state of the actor itself
-  request.post('http://' + actor.last_seen_host + '/actor/' + actor._id + '',{ json: {state:req.body.state} }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-
-      // if all goes well then update the db with the new state
-      console.log("Updating actor " + actor._id + " state to " + req.body.state );
-      actor.state = req.body.state;
-      // handle issues with conversion
-      actor.save(function(err,actor) {
-        if (err) {
-          console.log("Failed to update local actor state err: "+err);
-          res.status(404).json({success:false});
-        } else {
-          res.json({success:true, request:req.body });
-        }
-      });
-    } else {
-      console.log("Failed to update remote actor state");
-      res.status(404).json({success:false});
-    }
-  });
-}
-
-// this function should be run everytime the actor interacts with the hub
-function actorInteraction(actor_id, res) {
-  checkActorExists(actor_id, function(actor) {
-    actor.last_seen_time = new Date();
-    actor.save(function(err,actor) {
-      if (err) {
-        console.log("Failed to update last seen time err: "+err);
-        if (res) res.json({success:false});
-      } else {
-        if (res) res.json({success:true});
-      }
-    });
-  });
-}
-
-// shared functions
-function checkActorExists(actor_id,callback,res) {
-  Actor.findById(actor_id, function(err,actor){
-    if (err || !actor) {
-      console.log("Actor not registered err: "+err);
-      if (res) res.status(404).json({success:false,error:"Actor not registered"});
-    } else {
-      callback(actor);
-    }
-  });
-}
 
 router
 
@@ -81,6 +27,7 @@ router
   actor.name = req.body.name || "default name";
   actor.description = req.body.description || undefined;
   actor.last_seen_time = new Date();
+  actor.state_type = req.body.state_type;
   console.log("Tried to add a new actor: " + actor.id + ", name: " +actor.name + ", description: " +actor.description );
 
   // handle issues with conversion
@@ -89,7 +36,7 @@ router
       console.log("Failed to add actor err: "+err);
       res.json({success:false});
     } else {
-      actorInteraction(actor._id);
+      actor_helpers.ActorInteraction(actor._id);
       res.json({success:true, id:actor._id});
     }
   });
@@ -104,8 +51,9 @@ router
       res.status(404).json({success:false});
     } else {
       console.log("Updating actor: ", actor._id);
-      actor.name = req.body.name;
-      actor.description = req.body.description;
+      if (req.body.name) actor.name = req.body.name;
+      if (req.body.description) actor.description = req.body.description;
+      if (req.body.state_type) actor.state_type = req.body.state_type;
       console.log(" New actor name: "+actor.name + " description: "+actor.description );
       // handle issues with conversion
       actor.save(function(err,actor) {
@@ -150,7 +98,7 @@ router
 .post('/:actor_id/state', function(req, res, next) {
   checkActorExists(req.params.actor_id,function (actor) {
     actorInteraction(actor._id);
-    updateActorState(actor,req,res);
+    actor_helpers.PerformAction(actor,req.body.state,res);
   },res);
 })
 
